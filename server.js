e7c161488f2f79a445ae8cc722250c6c7a83bafb
ashu-choudhury <=>
2025-05-04 20:46:40 +0530

    demo

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from DevOps Assignment!'));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
