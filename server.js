730066e33077deb9701abed10dcf8717da1918d7
ashu-choudhury <=>
2025-05-04 23:15:55 +0530

    a

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 //app.use("/",express.static("public"));
 const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => res.send("server is working text"));
+app.get("/", (req, res) => res.send("server is working"));
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
