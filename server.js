28e8fd7a7b2519885302354908b39a2b1189c804
ashu-choudhury <=>
2025-05-04 23:21:50 +0530

    add html page

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const app = express();
-//app.use("/",express.static("public"));
+app.use("/",express.static("public"));
 const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => res.send("server is working"));
+//app.get("/", (req, res) => res.send("server is working"));
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
