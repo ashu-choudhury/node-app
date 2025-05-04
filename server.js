ba32fa54c52b997ee538e0e674d8c79c39e08156
ashu-choudhury <=>
2025-05-04 21:20:50 +0530

    rm ashu

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from DevOps Assignment! from ashu!'));
+app.get('/', (req, res) => res.send('Hello from DevOps Assignment!'));
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
